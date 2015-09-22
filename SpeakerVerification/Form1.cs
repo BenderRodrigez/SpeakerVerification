827e573eb721ebf67fa7e41dd61de853c95e5821
Равиль Гиниятуллин <mr.makoto.kun@gmail.com>
2015-09-22 22:47:24 +0400

    Add dynamical overview of winners neuron in learning

--- SpeakerVerification/Form1.cs
+++ SpeakerVerification/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 using HelpersLibrary.DspAlgorithms;
 using HelpersLibrary.LearningAlgorithms;
@@ -21,17 +20,17 @@ namespace SpeakerVerification
 {
     public partial class Form1 : Form
     {
-        public PlotView CodeBookPlotView;
-        public PlotView FeaturesTrainDataPlotView;
-        public PlotView FeatureTestDataPlotView;
+        private readonly PlotView _codeBookPlotView;
+        private readonly PlotView _featuresTrainDataPlotView;
+        private readonly PlotView _featureTestDataPlotView;
         private const WindowFunctions.WindowType Window = WindowFunctions.WindowType.Blackman;//тип применяемой оконной функции
         private VectorQuantization _vqCodeBook;
-        private KohonenNetwork network;
+        private KohonenNetwork _network;
 
         public Form1()
         {
             InitializeComponent();
-            CodeBookPlotView = new PlotView();
+            _codeBookPlotView = new PlotView();
             var model = new PlotModel
             {
                 PlotType = PlotType.XY,
@@ -46,13 +45,13 @@ namespace SpeakerVerification
                 Palette = OxyPalettes.Hot(200),
             };
             model.Axes.Add(linearColorAxis);
-            CodeBookPlotView.Model = model;
+            _codeBookPlotView.Model = model;
 
-            CodeBookPlotView.Dock = DockStyle.Fill;
-            codeBookGroupBox.Controls.Add(CodeBookPlotView);
+            _codeBookPlotView.Dock = DockStyle.Fill;
+            codeBookGroupBox.Controls.Add(_codeBookPlotView);
             
 
-            FeaturesTrainDataPlotView = new PlotView();
+            _featuresTrainDataPlotView = new PlotView();
             var model2 = new PlotModel
             {
                 PlotType = PlotType.XY,
@@ -67,11 +66,11 @@ namespace SpeakerVerification
                 Palette = OxyPalettes.Hot(200),
             };
             model2.Axes.Add(linearColorAxis1);
-            FeaturesTrainDataPlotView.Model = model2;
-            FeaturesTrainDataPlotView.Dock = DockStyle.Fill;
-            trainFeaturesGroupBox.Controls.Add(FeaturesTrainDataPlotView);
+            _featuresTrainDataPlotView.Model = model2;
+            _featuresTrainDataPlotView.Dock = DockStyle.Fill;
+            trainFeaturesGroupBox.Controls.Add(_featuresTrainDataPlotView);
 
-            FeatureTestDataPlotView = new PlotView();
+            _featureTestDataPlotView = new PlotView();
             var model3 = new PlotModel
             {
                 PlotType = PlotType.XY,
@@ -86,9 +85,9 @@ namespace SpeakerVerification
                 Palette = OxyPalettes.Hot(200),
             };
             model3.Axes.Add(linearColorAxis2);
-            FeatureTestDataPlotView.Model = model3;
-            FeatureTestDataPlotView.Dock = DockStyle.Fill;
-            featureTestGroupBox.Controls.Add(FeatureTestDataPlotView);
+            _featureTestDataPlotView.Model = model3;
+            _featureTestDataPlotView.Dock = DockStyle.Fill;
+            featureTestGroupBox.Controls.Add(_featureTestDataPlotView);
 
             for(var type = WindowFunctions.WindowType.Rectangular; type <= WindowFunctions.WindowType.Blackman; type++)
             {
@@ -99,31 +98,33 @@ namespace SpeakerVerification
 
         private bool[,] ProvideKohonenCom(double[][] trainingSet)
         {
-            var isWinner = new bool[8, 8];
-            var learningRadius = 8/2;
+            var outputLayerSize = (int) Math.Round(Math.Sqrt((int) vqSizeNumericUpDown.Value));
+            var isWinner = new bool[outputLayerSize, outputLayerSize];
+            var learningRadius = outputLayerSize / 2;
             var neigborhoodFunction = new GaussianFunction(learningRadius);
             const LatticeTopology topology = LatticeTopology.Hexagonal;
 
             var inputLayer = new KohonenLayer(10);
-            var outputLayer = new KohonenLayer(new Size(8, 8), neigborhoodFunction, topology);
+            var outputLayer = new KohonenLayer(new Size(outputLayerSize, outputLayerSize), neigborhoodFunction, topology);
             var connector = new KohonenConnector(inputLayer, outputLayer) {Initializer = new RandomFunction(0, 100)};
             outputLayer.SetLearningRate(0.2, 0.05d);
             outputLayer.IsRowCircular = false;
             outputLayer.IsColumnCircular = false;
-            network = new KohonenNetwork(inputLayer, outputLayer);
+            _network = new KohonenNetwork(inputLayer, outputLayer);
 
             var progress = 1;
-            network.BeginEpochEvent += (senderNetwork, args) => Array.Clear(isWinner, 0, isWinner.Length);
+            _network.BeginEpochEvent += (senderNetwork, args) => Array.Clear(isWinner, 0, isWinner.Length);
 
-            network.EndSampleEvent += delegate
+            _network.EndSampleEvent += delegate
             {
-                isWinner[network.Winner.Coordinate.X, network.Winner.Coordinate.Y] =
+                isWinner[_network.Winner.Coordinate.X, _network.Winner.Coordinate.Y] =
                     true;
             };
 
-            network.EndEpochEvent += delegate
+            _network.EndEpochEvent += delegate
             {
                 progressBar1.Value = ((progress++) * 100) / 500;
+                PlotWinnersNeurons(isWinner);
                 Application.DoEvents();
             };
             var trSet = new TrainingSet(trainingSet[0].Length);
@@ -131,7 +132,7 @@ namespace SpeakerVerification
             {
                 trSet.Add(new TrainingSample(x));
             }
-            network.Learn(trSet, 500);
+            _network.Learn(trSet, 500);
             return isWinner;
         }
         
@@ -163,6 +164,7 @@ namespace SpeakerVerification
                         else
                         {
                             var winners = ProvideKohonenCom(trainDataLpc);
+                            PlotWinnersNeurons(winners);
                         }
                         break;
                     case "ARC":
@@ -176,6 +178,7 @@ namespace SpeakerVerification
                         else
                         {
                             var winners = ProvideKohonenCom(trainDataArc);
+                            PlotWinnersNeurons(winners);
                         }
                         break;
                     case "MFCC":
@@ -189,6 +192,7 @@ namespace SpeakerVerification
                         else
                         {
                             var winners = ProvideKohonenCom(trainDataMfcc);
+                            PlotWinnersNeurons(winners);
                         }
                         break;
                     case "VTC":
@@ -202,6 +206,7 @@ namespace SpeakerVerification
                         else
                         {
                             var winners = ProvideKohonenCom(trainDataVtc);
+                            PlotWinnersNeurons(winners);
                         }
                         break;
                     default:
@@ -309,9 +314,9 @@ namespace SpeakerVerification
                 {
                     heatMap.Data[i, j] = featureSet[i][j];
                 }
-            FeatureTestDataPlotView.Model.Series.Clear();
-            FeatureTestDataPlotView.Model.Series.Add(heatMap);
-            FeatureTestDataPlotView.Model.InvalidatePlot(true);
+            _featureTestDataPlotView.Model.Series.Clear();
+            _featureTestDataPlotView.Model.Series.Add(heatMap);
+            _featureTestDataPlotView.Model.InvalidatePlot(true);
         }
 
         private void PlotTrainFeatureMatrix(double[][] featureSet)
@@ -330,9 +335,9 @@ namespace SpeakerVerification
                 {
                     heatMap.Data[i, j] = featureSet[i][j];
                 }
-            FeaturesTrainDataPlotView.Model.Series.Clear();
-            FeaturesTrainDataPlotView.Model.Series.Add(heatMap);
-            FeaturesTrainDataPlotView.Model.InvalidatePlot(true);
+            _featuresTrainDataPlotView.Model.Series.Clear();
+            _featuresTrainDataPlotView.Model.Series.Add(heatMap);
+            _featuresTrainDataPlotView.Model.InvalidatePlot(true);
         }
 
         private void PlotCodeBook(double[][] codeBook)
@@ -351,8 +356,29 @@ namespace SpeakerVerification
                 {
                     heatMap.Data[i, j] = codeBook[i][j];
                 }
-            CodeBookPlotView.Model.Series.Add(heatMap);
-            CodeBookPlotView.Model.InvalidatePlot(true);
+            _codeBookPlotView.Model.Series.Add(heatMap);
+            _codeBookPlotView.Model.InvalidatePlot(true);
+        }
+
+        private void PlotWinnersNeurons(bool[,] winners)
+        {
+            var outputLayerSize = (int)Math.Round(Math.Sqrt((int)vqSizeNumericUpDown.Value));
+            var heatMap = new HeatMapSeries
+            {
+                Data = new double[outputLayerSize, outputLayerSize],
+                X0 = 0,
+                X1 = outputLayerSize,
+                Y0 = 0,
+                Y1 = outputLayerSize,
+                Interpolate = false
+            };
+            for (int i = 0; i < outputLayerSize; i++)
+                for (int j = 0; j < outputLayerSize; j++)
+                {
+                    heatMap.Data[i, j] = winners[i, j] ? 1.0 : 0.0;
+                }
+            _codeBookPlotView.Model.Series.Add(heatMap);
+            _codeBookPlotView.Model.InvalidatePlot(true);
         }
 
         private void SaveDistortionEnergyToFile(string fileName, double[][] testData)
@@ -369,11 +395,11 @@ namespace SpeakerVerification
                     }
                     else
                     {
-                        var win = network.Run(testData[i]);
+                        _network.Run(testData[i]);
                         var place = new double[testData[0].Length];
-                        for (int j = 0; j < network.Winner.SourceSynapses.Count; j++)
+                        for (int j = 0; j < _network.Winner.SourceSynapses.Count; j++)
                         {
-                            place[j] = network.Winner.SourceSynapses[j].Weight;
+                            place[j] = _network.Winner.SourceSynapses[j].Weight;
                         }
                         var distortion = VectorQuantization.QuantizationError(place, testData[i]);
                         writer.WriteLine(distortion);
